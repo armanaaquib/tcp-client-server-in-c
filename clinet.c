52282a3d0fb1630345acbca329127a745dbf033c
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-14 20:07:18 +0530

    Client working

--- clinet.c
+++ clinet.c
@@ -5,15 +5,23 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(void)
+#define HOST_ADDR "127.0.0.1"
+#define PORT "8000"
+
+struct sockaddr_in get_server(void)
 {
-  struct sockaddr_in server_addr;
-  bzero(&server_addr, sizeof(server_addr));
+  struct sockaddr_in server;
+  bzero(&server, sizeof(server));
 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  server_addr.sin_port = htons(5000);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr(HOST_ADDR);
+  server.sin_port = htons(atoi(PORT));
 
+  return server;
+}
+
+int create_connection()
+{
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sockfd < 0)
@@ -22,31 +30,37 @@ int main(void)
     exit(1);
   }
 
-  if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  const struct sockaddr_in server = get_server();
+
+  if (connect(sockfd, (const struct sockaddr *)&server, sizeof(server)) < 0)
   {
-    perror("ERROR connecting");
-    exit(1);
+    perror("Could not connect to Server at " HOST_ADDR ":" PORT);
+    return -1;
   }
 
-  int n = write(sockfd, "GET / HTTP/1.0\nhost: localhost\nUser-Agent: arman/1.0\nAccept: */*\n\n", 71);
-  if (n < 0)
+  return sockfd;
+}
+
+int main(void)
+{
+  int sockfd = create_connection();
+
+  while (1)
   {
-    perror("ERROR writing to socket");
-    exit(1);
+    int buffer_size = 255;
+    char buffer[buffer_size];
+    bzero(buffer, buffer_size);
+
+    printf("Enter your name: ");
+    gets(buffer);
+
+    send(sockfd, buffer, strlen(buffer), 0);
+
+    bzero(buffer, buffer_size);
+    recv(sockfd, buffer, buffer_size - 1, 0);
+
+    printf("%s\n", buffer);
   }
-
-  char buffer[256];
-  bzero(buffer, 256);
-
-  n = read(sockfd, buffer, 255);
-
-  if (n < 0)
-  {
-    perror("ERROR reading from socket");
-    exit(1);
-  }
-
-  printf("%s\n", buffer);
 
   return 0;
 }
