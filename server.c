d4b064bd6b36b52416ea968282ee3a1cd3fc26f5
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-08-06 19:58:17 +0530

    Added hanldle request in server.c

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <netdb.h>
 
 #define PORT "8000"
+#define IPCOMP(addr, n) ((addr >> (24 - 8 * n)) & 0xFF)
 
 struct sockaddr_in get_serveraddr(void)
 {
@@ -35,7 +36,25 @@ int start_listening(const struct sockaddr_in serveraddr, int no_of_connections)
   }
 
   listen(sockfd, no_of_connections);
+  printf("Listening At: " PORT "\n");
+
   return sockfd;
+}
+
+void handle_request(int sockfd, const struct sockaddr_in addr)
+{
+  while (1)
+  {
+    char buffer[256];
+    bzero(buffer, 256);
+
+    recv(sockfd, buffer, 255, 0);
+
+    printf("Got %s\n", buffer);
+    printf("From %d.%d.%d.%d:%d\n", IPCOMP(addr.sin_addr.s_addr, 0), IPCOMP(addr.sin_addr.s_addr, 1), IPCOMP(addr.sin_addr.s_addr, 2), IPCOMP(addr.sin_addr.s_addr, 3), addr.sin_port);
+
+    send(sockfd, "Got your response.", 5, 0);
+  }
 }
 
 int main(void)
@@ -44,31 +63,29 @@ int main(void)
 
   struct sockaddr_in client_addr;
   socklen_t client_addr_size = sizeof(client_addr);
-  int c_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_size);
+  bzero(&client_addr, client_addr_size);
 
-  if (c_sockfd < 0)
+  while (1)
   {
-    perror("ERROR on accept");
-    exit(1);
-  }
+    int c_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_size);
 
-  char buffer[256];
-  bzero(buffer, 256);
+    if (c_sockfd < 0)
+    {
+      perror("ERROR on accept");
+      exit(1);
+    }
 
-  int n = recv(c_sockfd, buffer, 255, 0);
-  if (n < 0)
-  {
-    perror("ERROR reading from socket");
-    exit(1);
-  }
+    if (fork() == 0)
+    {
+      close(sockfd);
+      handle_request(c_sockfd, client_addr);
+    }
+    else
+    {
+      close(c_sockfd);
+    }
 
-  printf("%s\n", buffer);
-
-  n = send(c_sockfd, "Hello", 5, 0);
-  if (n < 0)
-  {
-    perror("ERROR writing to socket");
-    exit(1);
+    bzero(&client_addr, client_addr_size);
   }
 
   return 0;
